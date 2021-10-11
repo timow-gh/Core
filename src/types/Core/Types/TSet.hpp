9aabd969219154ca1c32183c7e239727027858bc
timow-gh <39085531+timow-gh@users.noreply.github.com>
2021-10-11 20:40:23 +0200

    Some compilation tests

--- src/types/Core/Types/TSet.hpp
+++ src/types/Core/Types/TSet.hpp
@@ -5,8 +5,10 @@
 
 namespace Core
 {
-template <typename T, typename Alloc = std::allocator<T>>
-using TSet = std::set<T, Alloc>;
-}
+template <typename T,
+          typename Compare = std::less<T>,
+          typename Alloc = std::allocator<T>>
+using TSet = std::set<T, Compare, Alloc>;
+} // namespace Core
 
 #endif // CORE_TSET_HPP
