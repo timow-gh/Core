dab9ce9f00f6ec0a032e21459e138d0ccb61a95d
timow-gh <39085531+timow-gh@users.noreply.github.com>
2022-02-12 17:23:48 +0100

    Added Constexpr

--- src/math/Core/Math/Eps.hpp
+++ src/math/Core/Math/Eps.hpp
@@ -1,16 +1,17 @@
 #ifndef CORE_EPS_H
 #define CORE_EPS_H
 
+#include <Core/Utils/Compiler.hpp>
 #include <cmath>
 
 namespace Core
 {
 
-constexpr float_t EPS_F = 1.0E-6;
-constexpr double_t EPS_D = 1.0E-15;
+CORE_CONSTEXPR float_t EPS_F = 1.0E-6;
+CORE_CONSTEXPR double_t EPS_D = 1.0E-15;
 
-constexpr float_t ZERO_F = 0.0f;
-constexpr double_t ZERO_D = 0.0;
+CORE_CONSTEXPR float_t ZERO_F = 0.0f;
+CORE_CONSTEXPR double_t ZERO_D = 0.0;
 
 template <typename T>
 struct eps_traits;
@@ -18,83 +19,83 @@ struct eps_traits;
 template <>
 struct eps_traits<float_t>
 {
-    static constexpr float_t value() { return EPS_F; };
+    static CORE_CONSTEXPR float_t value() { return EPS_F; };
 };
 
 template <>
 struct eps_traits<double_t>
 {
-    static constexpr double_t value() { return EPS_D; };
+    static CORE_CONSTEXPR double_t value() { return EPS_D; };
 };
 
 template <typename T>
-constexpr bool isZero(T value, T eps)
+CORE_CONSTEXPR bool isZero(T value, T eps)
 {
     return std::abs(value) < eps;
 }
 
 template <typename T>
-constexpr bool isZero(T value)
+CORE_CONSTEXPR bool isZero(T value)
 {
     return isZero(value, eps_traits<T>::value());
 }
 
 template <typename T>
-constexpr bool isLess(T lhs, T rhs, T eps)
+CORE_CONSTEXPR bool isLess(T lhs, T rhs, T eps)
 {
     return lhs + eps < rhs;
 }
 
 template <typename T>
-constexpr bool isLess(T lhs, T rhs)
+CORE_CONSTEXPR bool isLess(T lhs, T rhs)
 {
     return isLess(lhs, rhs, eps_traits<T>::value());
 }
 
 template <typename T>
-constexpr bool isLessEq(T lhs, T rhs, T eps)
+CORE_CONSTEXPR bool isLessEq(T lhs, T rhs, T eps)
 {
     return !(isLess(rhs, lhs, eps));
 }
 
 template <typename T>
-constexpr bool isLessEq(T lhs, T rhs)
+CORE_CONSTEXPR bool isLessEq(T lhs, T rhs)
 {
     return isLessEq(lhs, rhs, eps_traits<T>::value());
 }
 
 template <typename T>
-constexpr bool isGreater(T lhs, T rhs, T eps)
+CORE_CONSTEXPR bool isGreater(T lhs, T rhs, T eps)
 {
     return isLess(rhs, lhs, eps);
 }
 
 template <typename T>
-constexpr bool isGreater(T lhs, T rhs)
+CORE_CONSTEXPR bool isGreater(T lhs, T rhs)
 {
     return isGreater(lhs, rhs, eps_traits<T>::value());
 }
 
 template <typename T>
-constexpr bool isGreaterEq(T lhs, T rhs, T eps)
+CORE_CONSTEXPR bool isGreaterEq(T lhs, T rhs, T eps)
 {
     return !isLess(lhs, rhs, eps);
 }
 
 template <typename T>
-constexpr bool isGreaterEq(T lhs, T rhs)
+CORE_CONSTEXPR bool isGreaterEq(T lhs, T rhs)
 {
     return isGreaterEq(lhs, rhs, eps_traits<T>::value());
 }
 
 template <typename T>
-constexpr bool isEq(T lhs, T rhs, T eps)
+CORE_CONSTEXPR bool isEq(T lhs, T rhs, T eps)
 {
     return std::abs(lhs - rhs) < eps;
 }
 
 template <typename T>
-constexpr bool isEq(T lhs, T rhs)
+CORE_CONSTEXPR bool isEq(T lhs, T rhs)
 {
     return isEq(lhs, rhs, eps_traits<T>::value());
 }
