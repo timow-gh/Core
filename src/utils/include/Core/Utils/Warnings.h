e5a2e8824a23ac26bbb6f88cee9e90426e7de4d3
timow-gh <39085531+timow-gh@users.noreply.github.com>
2022-06-19 16:20:38 +0200

    Added new cmake option Core_USE_WARNINGS

---
+++ src/utils/include/Core/Utils/Warnings.h
@@ -0,0 +1,26 @@
+#ifndef MESHLER_WARNINGS_H
+#define MESHLER_WARNINGS_H
+
+#define DO_PRAGMA(X) _Pragma(#X)
+
+#if defined(_MSC_VER)
+#define DISABLE_ALL_WARNINGS DO_PRAGMA(warning(push, 0))
+#elif defined(__clang__)
+#define DISABLE_ALL_WARNINGS                                                   \
+    DO_PRAGMA(clang diagnostic push)                                           \
+    DO_PRAGMA(clang diagnostic ignored "-Weverything")
+#elif defined(__GNUC__)
+#define DISABLE_ALL_WARNINGS                                                   \
+    DO_PRAGMA(GCC diagnostic push)                                           \
+    DO_PRAGMA(GCC diagnostic ignored "-Weverything")
+#endif
+
+#if defined(_MSC_VER)
+#define ENABLE_ALL_WARNINGS _Pragma warning(pop)
+#elif defined(__clang__)
+#define ENABLE_ALL_WARNINGS DO_PRAGMA(clang diagnostic pop)
+#elif defined(__GNUC__)
+#define ENABLE_ALL_WARNINGS DO_PRAGMA(GCC diagnostic pop)
+#endif
+
+#endif // MESHLER_WARNINGS_H
